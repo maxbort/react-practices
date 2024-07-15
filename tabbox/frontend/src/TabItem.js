99c2e129b29381f489d976a3474c77c83b94ec95
CHOI HyoJun <cjfgh2449@gmail.com>
2024-07-15 16:31:01 +0900

    tabbox #finish

--- tabbox/frontend/src/TabItem.js
+++ tabbox/frontend/src/TabItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function TabItem({name, active}) {
     return (
-        <li className="active">
-            {name}
-        </li>
+        <li className={active ? 'active' : ''}>{name}</li>
     );
 }
+
+
 export default TabItem;
--- tabbox/frontend/src/index.js
+++ tabbox/frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // React 18부터 도입된 새로운 렌더링 진입점.
+                                                                  // 렌더링 시작할 DOM 요소 지정
+root.render( // React 엘리멘트를 지정된 루트 DOM 요소에 렌더링하는 역할
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+// React 어플리케이션 시작하는데 사용되는 진입점의 역할.
+// ReactDom.createRoot()
