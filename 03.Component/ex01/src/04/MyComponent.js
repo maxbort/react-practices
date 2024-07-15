4781d146b07cccb21f6293948d032566c8f77831
CHOI HyoJun <cjfgh2449@gmail.com>
2024-07-15 16:47:48 +0900

    props Validation

--- 03.Component/ex01/src/04/MyComponent.js
+++ 03.Component/ex01/src/04/MyComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
-
 function MyComponent({props01, props02, props03, props04, props05, props06, props07, props08, props09}) {
     return (
         <div>
